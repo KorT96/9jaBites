82ce84e26e422f78a24a50b2bc8fbc245713fdf4
KorT96 <asasaprankz1994@gmail.com>
2026-06-28 14:32:50 +0100

    Animated page

--- 9jaBites/app/(app)/(public)/index.tsx
+++ 9jaBites/app/(app)/(public)/index.tsx
@@ -1,8 +1,10 @@
 import AppleAuthButton from "@/components/auth/AppleAuthButton";
 import GoogleAuthButton from "@/components/auth/GoogleAuthButton";
+import SmoothInfiniteScroll from "@/components/SmoothInfiniteScroll";
 import { Fonts } from "@/constants/theme";
 import { StyleSheet, Text, View, Image, TouchableOpacity, Linking } from "react-native";
 import Animated, { FadeInDown } from 'react-native-reanimated';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Index() {
   const openWebBrowser = () => {
@@ -12,7 +14,28 @@ export default function Index() {
     };
   return (
     <View style={styles.container}>
-      <View style={styles.scrollContainer} />
+      <View style={styles.scrollContainer}>
+        <View>
+          <SmoothInfiniteScroll scrollDirection="down" iconSet="set1" />
+        </View>
+        <View>
+          <SmoothInfiniteScroll scrollDirection="up" iconSet="set2" />
+        </View>
+        <View>
+          <SmoothInfiniteScroll scrollDirection="down" iconSet="set3" />
+        </View>
+        <LinearGradient
+          colors={['transparent', '#fff']}
+          style={{
+            position: 'absolute',
+            height: 200,
+            left: 0,
+            bottom: 0,
+            right: 0,
+          }}
+        />
+      </View>
+
       <View style={styles.contentContainer}>
         <Image source={require('@/assets/images/logo.png')} style={styles.brandlogo}/>
         <Animated.Text entering={FadeInDown} style={styles.tag}>
@@ -88,6 +111,12 @@ const styles = StyleSheet.create({
   },
   scrollContainer: {
     flex: 0.4, // Adjusted layout distribution
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center', 
+    gap: 4,
+    position: 'relative',
+    overflow: 'hidden',
   },
   privacyContainer: {
     marginTop:30,
